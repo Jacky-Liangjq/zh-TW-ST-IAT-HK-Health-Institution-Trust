164d3eafa0e8aa3a7cb39c863a76760b03d8045c
Jacky-Liangjq <jacky.leong.ljq@gmail.com>
2026-02-23 14:56:06 +0800

    Update HKHealthTrustSTIAT-mobile.js

--- HKHealthTrustSTIAT-mobile.js
+++ HKHealthTrustSTIAT-mobile.js
@@ -1,7 +1,7 @@
 define(
   [
     'pipAPI',
-    'https://cdn.jsdelivr.net/gh/Jacky-Liangjq/zh-TW-ST-IAT-HK-Health-Institution-Trust@12bf30982ac18b54d3718d0fb33220b5f2066c5c/qstiat6-mobile.js'
+    'https://cdn.jsdelivr.net/gh/Jacky-Liangjq/zh-TW-ST-IAT-HK-Health-Institution-Trust@7db87eabf1d2a9da5105ed955cacf722cdd04789/qstiat6-mobile.js'
   ],
   function (APIConstructor, stiatExtension) {
 
