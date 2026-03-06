2b0d40a80bd0303e34da88f5f2f9c3b1ca5bf048
Jacky-Liangjq <jacky.leong.ljq@gmail.com>
2026-03-06 16:23:24 +0800

    Update HKHealthTrustSTIAT-mobile.js

--- HKHealthTrustSTIAT-mobile.js
+++ HKHealthTrustSTIAT-mobile.js
@@ -133,7 +133,7 @@ define(
         { instHTML: instHTML, block:1, miniBlocks:4, singleAttTrials:7, sharedAttTrials:5, categoryTrials:0 },
       
         // Block2
-        { instHTML: instHTML, block:2, miniBlocks:4, singleAttTrials:6, sharedAttTrials:6, categoryTrials:0 },
+        { instHTML: instHTML, block:2, miniBlocks:4, singleAttTrials:8, sharedAttTrials:7, categoryTrials:0 },
       
         // Block3
         { instHTML: instHTML, block:3, miniBlocks:3, singleAttTrials:3, sharedAttTrials:3, categoryTrials:3 },
